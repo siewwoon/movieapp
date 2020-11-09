f2fbf3c5a958b51068dfeb0d0ef739c34dd75713
siewwoon <siewwoon.lee@gmail.com>
2020-11-09 19:52:45 +0800

    second commit

--- src/components/pages/Comedy.js
+++ src/components/pages/Comedy.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState} from "react";
+import Movie from '../Movie';
 
-export default function Comedy () {
-    return (
-        <>
+const Featured_API = "https://api.themoviedb.org/3/discover/movie?api_key=bee8ae0e180f15cce41dace403016b99&with_genres=35";
+
+export default function Consulting () {
+    const [ movies, setMovies] = useState([]);
+
+
+ useEffect(()=> {
+  getMovies (Featured_API); }, []);
+
+ const getMovies = (API) => {
+  fetch (API)
+  .then ((res) => res.json())
+  .then ((data) => {
+      console.log(data);
+      setMovies(data.results);
+  });
+ }
+
+ return (
+    <>
 <h1 className="comedy">Comedy Movie </h1>
-        </>
-    )
+<div className="movie-container">
+  {movies.length > 0 && movies.map((movie) =>
+ <Movie key={movie.id} {...movie} />)};
+ </div>
+    </>
+);
 }
